25de36b4d2156a217a648ec9813efb6ac7b4acdd
Syeda Marium Fatima <syedamariumfatima20@gmail.com>
2026-04-26 19:00:47 +0500

    Delete 2nd Semester directory

--- 2nd Semester/Review.cpp
+++
@@ -1,9 +0,0 @@
-#include "Review.h"
-
-Review::Review(string name, string c, int r) : reviewerName(name), comments(c), rating(r) {}
-
-void Review::displayReview() const
-{
-	cout << reviewerName << " rated " << rating << "/5" << endl;
-	cout << "Comments: " << comments << endl;
-}
